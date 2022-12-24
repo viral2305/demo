c232479a6f98e4b216eedc72f737521f4529387a
viral dudhat <viraldudhat522@gmail.com>
2022-12-24 15:53:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//constant for snack game
+let inputDir = { x: 0, y: 0 };
+const backgroundSong = new Audio('music/music.mp3');
+
+function main(ctime) {
+  console.log('ctime', ctime);
+}
+
+window.requestAnimationFrame(main);
+window.addEventListener('keydown', (e) => {
+  const x = Math.random();
+  inputDir = { x: 3, y: 3 };
+});
